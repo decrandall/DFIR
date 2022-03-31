dc8a0a684494438b83eebdc21fcc8bf8b3da4572
decrandall <102704056+decrandall@users.noreply.github.com>
2022-03-30 19:14:21 -0500

    Update Kansa-Fixes.ps1

--- Kansa-Fixes.ps1
+++ Kansa-Fixes.ps1
@@ -10,10 +10,31 @@ Place this ps1 in a directory with as many autorunsc files as you can feed it.
 
 $autorunscCompiled = Get-ChildItem | ?{$_.Name -like "*Autorunsc.csv"} | %{import-csv $_.FullName} 
 
-$groupedresults = $autorunscCompiled | ?{$_.Signer -notlike "(Verified)*" -and $_."Image Path" -notlike "File not found*"}| group "Image Path","Launch String","Md5","Signer" 
+$groupedresults = $autorunscCompiled | ?{$_.Signer -notlike "(Verified)*" -and $_."Image Path" -notlike "File not found*"}| group "Image Path","Launch String","Md5"
 
 $results = $groupedresults |  Select @{n="ct";e={$_.count}}, @{n="Image Path";e={@($_.group."Image Path")[0]}},@{n="Launch String";e={@($_.group."Launch String")[0]}},@{n="MD5";e={@($_.group."MD5")[0]}},@{n="Signer";e={@($_.group."Signer")[0]}}
 
 #change the output file name below if you like
 
 $results | sort ct | Export-Csv -NoTypeInformation .\asep-workstation-stack.csv
+
+
+<#
+
+Kansa Script Get-ASEPImagePathLaunchStringMD5UnsignedTimeStack.ps1 needs a dependency "logparser.exe" and it's not really needed
+
+Use this native Powershell version instead
+
+Place this ps1 in a directory with as many autorunsc files as you can feed it.
+
+#>
+
+$autorunscCompiled = Get-ChildItem | ?{$_.Name -like "*Autorunsc.csv"} | %{import-csv $_.FullName} 
+
+$groupedresults = $autorunscCompiled | ?{$_.Signer -notlike "(Verified)*" -and $_."Image Path" -notlike "File not found*"}| group "Image Path","Launch String","Md5"
+
+$results = $groupedresults |  Select @{n="ct";e={$_.count}},@{n="Time";e={@($_.group."Time")[0]}}, @{n="Image Path";e={@($_.group."Image Path")[0]}},@{n="Launch String";e={@($_.group."Launch String")[0]}},@{n="MD5";e={@($_.group."MD5")[0]}},@{n="Signer";e={@($_.group."Signer")[0]}}
+
+#change the output file name below if you like
+
+$results | sort ct | Export-Csv -NoTypeInformation .\asep-workstation-timestack.csv
